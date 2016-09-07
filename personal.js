7fa295eaa5e68ebc34ff431b96e042b9e59acee6
YuguangLee <lli@adobe.com>
2016-09-06 19:19:07 -0700

    Fix overlapping

--- personal.js
+++ personal.js
@@ -2,3 +2,21 @@
  * Created by Yuguang Li on 9/3/16.
  */
 
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+});
